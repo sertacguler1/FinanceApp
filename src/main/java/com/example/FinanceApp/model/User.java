a42fb3c944a413b5d723ac111accf985de47a0f1
Sertac Guler <56694619+sertacguler1@users.noreply.github.com>
2025-06-04 10:32:34 +0300

    Fix bean cycle and database schema issues

--- src/main/java/com/example/FinanceApp/model/User.java
+++ src/main/java/com/example/FinanceApp/model/User.java
@@ -20,7 +20,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true, nullable = false)
+    @Column(unique = true)
     private String username;
 
     @Column(nullable = false)
